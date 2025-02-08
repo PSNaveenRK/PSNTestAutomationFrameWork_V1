9fd46dc425fc1d96ab9f4d9c9297f53682eb12e8
PSNaveenRK <90152984+PSNaveenRK@users.noreply.github.com>
2025-02-08 15:35:34 +0530

    PSNTestAutomationFW_V1Only Selenium and Java

---
+++ PSN_Test_Automation_V1/src/Test/TestRunner.java
@@ -0,0 +1,19 @@
+package Test;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class TestRunner {
+
+	public static void main(String[] args) {
+		
+		Automation_FrameWork_Methods FrameWorkObj = new Automation_FrameWork_Methods();
+		FrameWorkObj.invokeDriver("chrome", "");
+		String myurl = "https://psnaveenrk.github.io/Resume";
+		FrameWorkObj.launchWebApp(myurl);
+		String myurl2 = "https://google.com";
+		FrameWorkObj.launchAppInNewTab(myurl2);
+		FrameWorkObj.quitWebApp();
+	}
+
+}
